8f3b3af11411ca44d211b208d1881f92ccb550e7
NK972 <nicolas.castry@cpe.fr>
2018-03-31 12:37:40 +0200

    Signed-off-by: NK972 <nicolas.castry@cpe.fr>

--- 1_BSE_Main.c
+++ 1_BSE_Main.c
@@ -1,4 +1,4 @@
-#include <c8051f020.h>
+﻿#include <c8051f020.h>
 #include "Declarations_GPIO_BSE.h"
 #include "LIB_BSE_Timers.h"
 #include "LIB_BSE_Config_Globale.h"
@@ -12,10 +12,10 @@ void main()
 {
 	char* s = "Salut";
 	Init_Device(); // Regroupe toutes les fonctions de configuration
-	/*
+
 	send_String(s);// Envoie la chaine de caratère Via l'UART0.
 	Getchar(); 		 // Recupère le caractère de l'UART0.
-	*/
+
 	while(1){
 		ACQ_ADC();
 		
